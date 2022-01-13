c423109317546893874ef5730253e981f28cad7f
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2022-01-13 13:11:54 +0530

    Update pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -13,6 +13,13 @@ int main(){
     // * --> Dereference( Address of) operator
 
     cout<<"The value at address b is"<<*b<<endl;
+    
+    // Pointer to pointer
+    int **c=&b;
+    cout<<"The value of b is"<<&b<<endl;
+    cout<<"The address of b is"<<c<<endl;
+    cout<<"The value at address c is"<<*c<<endl;
+    cout<<"The value at address value_at(value_at(c)) is"<<**c<<endl;
 
 
 return 0;
