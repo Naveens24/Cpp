b1420e14b27f31695dc2ef114d78f2d71270444d
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2022-02-05 17:24:12 +0530

    Create pointer_to_obj_arrow.cpp

---
+++ pointer_to_obj_arrow.cpp
@@ -0,0 +1,36 @@
+// Pointers to object and arrow
+
+#include<iostream>
+using namespace std;
+
+class Complex{
+    int real , imaginary;
+    public:
+        void getData(){
+            cout<<"The real part is "<<real<<endl;
+            cout<<"The imaginary part is "<<imaginary<<endl;
+        }
+
+        void setData(int a, int b){
+            real = a; 
+            imaginary = b;
+
+        }
+};
+int main(){
+    // Complex c1;
+    // Complex *ptr = &c1;
+    Complex *ptr = new Complex;
+    // (*ptr).setData(1, 34);  is exactrly same as
+    ptr->setData(1, 42);
+
+    // (*ptr).getData(); is as good as
+    ptr->getData();
+    
+
+    // Array of objects
+    Complex *ptr1 = new Complex[4];
+    ptr1->setData(1,4);
+    ptr1->getData();
+return 0;
+}
