2b95c1ea72932593c75671a0c138acbdf07331bf
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2022-02-06 19:11:43 +0530

    Create this_pointer53.cpp

---
+++ this_pointer53.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// Polymorphism -> ek chiz ke multiple form
+//   - One name and multiple forms
+//   - eg. Function overloading, Operator overloading
+//   - eg. virtual function
+
+int main(){
+    /*
+    Polymorphism in C++ can be of two types:
+    1. Compile time polmorphism
+        Compile time poymorphism in C++ is achieved using:
+        1.1 - Function overloading
+        1.2 - Operator overloading
+    
+    2. Run time poymorphism
+        2.1 - Virtual functions
+    
+    */
+return 0;
+}
